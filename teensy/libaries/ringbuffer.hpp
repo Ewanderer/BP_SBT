2efeed37bd70d6e472b0e6591e6cf0ec5c76eba4
Jordan Eichner <jordan.eichner@gmx.de>
2016-12-13 14:46:38 +0100

    other documents that were writtin on the lab PC
    Because the lab PC doesn't have git on it...

---
+++ teensy/libaries/ringbuffer.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+template<class T,unsigned size>
+class ringbuffer
+{
+	
+	T buffer[size];
+	unsigned head, tail;
+public:
+	ringbuffer(){
+		head = 0;
+		tail = 0;
+		for (int i = 0; i < size;i++)
+			buffer[i]=T();
+	}
+	
+	//returns true if sucessful
+	bool push(T element){
+		if ((head+1) % (size+1) != tail){
+			buffer[head] = element;
+			head = (head + 1) % (size+1);
+			return true;
+		}
+		return false;
+	}
+
+	//return true if sucessful
+	bool pop(T* ref){
+		if (head != tail){
+			*ref = buffer[tail];
+			tail = (tail + 1) % (size+1);
+			return true;
+		}
+		return false;
+		
+	}
+
+};
+
